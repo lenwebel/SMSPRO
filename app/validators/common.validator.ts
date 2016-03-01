0c66c0b6cb6b5ba9f09518cfc6ae496ebc091606
Leonard Webel <leonard.webel@satalyst.com>
2016-03-02 07:21:42 +0800

    validator

---
+++ app/validators/common.validator.ts
@@ -0,0 +1,16 @@
+import * as validation from '../interfaces/validationresult'
+import {Control} from 'angular2/common'
+
+class CommonValidator {
+  
+  
+     static startsWithNumber(control: Control): validation.ValidationResult { 
+ 
+   if ( control.value != "" && !isNaN(control.value.charAt(0)) ){
+     return { "startsWithNumber": true };
+   }
+ 
+   return null;
+ }
+    
+}
